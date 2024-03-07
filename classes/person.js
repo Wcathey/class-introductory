9dfc521ef62dde9875de0b5d9f298f193f6ed0a2
William <wcathey1604@gmail.com>
2024-03-07 13:21:30 -0600

    passing all specs

--- classes/person.js
+++ classes/person.js
@@ -1,5 +1,28 @@
 class Person {
-  constructor(firstName)
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople(arr) {
+    if(!Array.isArray(arr)) {
+      throw Error("introducePeople only takes an array as an argument.")
+
+    }
+    for(let pupil of arr) {
+      if(pupil instanceof Person === false) {
+        throw Error("All items in array must be Person class instances.")
+      }
+    }
+    arr.forEach(person => {
+      person.introduce();
+    })
+  }
 }
 
 /****************************************************************************/
