6922be95b5afb1a79f70c67b511a1c7ea5a22ec8
William <wcathey1604@gmail.com>
2024-03-07 12:42:41 -0600

    first commit

---
+++ classes/person.js
@@ -0,0 +1,12 @@
+class Person {
+  constructor(firstName)
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Person;
+} catch {
+  module.exports = null;
+}
